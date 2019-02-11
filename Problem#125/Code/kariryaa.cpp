c8a933f5b101e7d32a459ae93635dfe8df6c180a
Pulkit Kumar Kariryaa <kariryap@in.ibm.com>
2019-02-11 15:18:46 +0530

    solution for problem 125

---
+++ Problem#125/Code/kariryaa.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int len = s.size();
+        for(int i=0,j=len-1;i<=j;)
+        {
+            if(!isalnum(s[i]))
+                i++;
+            else if(!isalnum(s[j]))
+                j--;
+            else
+                if(tolower(s[i++]) != tolower(s[j--])) return false;
+        }
+        return true;
+    }
+};

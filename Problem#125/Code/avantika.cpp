c4803de6026b55c8f2b92e3a00f965295e80a11b
avchhabr <avchhabr@microsoft.com>
2019-02-11 16:49:09 +0530

    Add solution for 125

---
+++ Problem#125/Code/avantika.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int t = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[t] = nums[i];
+                t++;
+            }
+        }
+        for (int i = t; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};

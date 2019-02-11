86aec79e05b4215696c68818b903adb4ca1f9743
Lalit Dudheria <lalit.dudheria@gmail.com>
2019-02-11 18:39:38 +0530

    Create lalit.cpp

---
+++ Problem#344/Code/lalit.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int n=s.size();
+        
+        if (n==0 || n==1)
+            return;
+        
+        int i=0, j=n-1;
+        
+        while(i<=j)
+        {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        
+    }
+};
